8903c0018dea55b90bf6f181b4b6024927f795cc
Anthony <anthonymarques@outlook.com>
2021-11-19 22:28:50 +0200

    need to work on styles and layout hard core Just adjusted table arragement much better now

--- src/Home.vue
+++ src/Home.vue
@@ -1,43 +1,45 @@
 <template>
   <div class="container">
-    <table class="table">
-      <thead>
-        <tr>
-          <th scope="col">key</th>
-          <th scope="col">Description</th>
-          <th scope="col">dst_host</th>
-          <th scope="col">src_host</th>
-          <th scope="col">Created on</th>
-          <th scope="col">Node ID</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="alert in alerts" v-bind:key="alert.key">
-          <th>{{ alert.key }}</th>
-          <th>{{ alert.description }}</th>
-          <th>{{ alert.dst_host }}</th>
-          <th>{{ alert.src_host }}</th>
-          <th>{{ alert.created }}</th>
-          <th>{{ alert.node_id }}</th>
-        </tr>
-      </tbody>
-    </table>
-    <table class="table">
-      <thead>
-        <tr>
-          <th scope="col">Node ID</th>
-          <th scope="col">Description</th>
-          <th scope="col">ip Address</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="device in device_list" v-bind:key="device.node_id">
-          <th>{{ device.node_id }}</th>
-          <th>{{ device.description }}</th>
-          <th>{{ device.ip_address }}</th>
-        </tr>
-      </tbody>
-    </table>
+    <div class="table-holder">
+      <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">Node ID</th>
+            <th scope="col">Description</th>
+            <th scope="col">ip Address</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="device in device_list" v-bind:key="device.node_id">
+            <th>{{ device.node_id }}</th>
+            <th>{{ device.description }}</th>
+            <th>{{ device.ip_address }}</th>
+          </tr>
+        </tbody>
+      </table>
+      <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">key</th>
+            <th scope="col">Description</th>
+            <th scope="col">dst_host</th>
+            <th scope="col">src_host</th>
+            <th scope="col">Created on</th>
+            <th scope="col">Node ID</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="alert in alerts" v-bind:key="alert.key">
+            <th>{{ alert.key }}</th>
+            <th>{{ alert.description }}</th>
+            <th>{{ alert.dst_host }}</th>
+            <th>{{ alert.src_host }}</th>
+            <th>{{ alert.created }}</th>
+            <th>{{ alert.node_id }}</th>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 
@@ -48,8 +50,8 @@ export default {
   data() {
     return {
       consolData: [],
-      alerts:[],
-      device_list:[]
+      alerts: [],
+      device_list: [],
     };
   },
   created: function () {
@@ -58,7 +60,7 @@ export default {
         "https://thinkst-frontend-resources.s3-eu-west-1.amazonaws.com/incidents/data.json"
       )
       .then((res) => {
-          console.log(res);
+        console.log(res);
         this.consolData = res.data;
         this.alerts = this.consolData.alerts;
         this.device_list = this.consolData.device_list;
@@ -69,10 +71,11 @@ export default {
 </script>
 <style scoped>
 .container {
-    display: flex;
+  justify-content: space-around;
 }
 
-.container table {
-    flex-direction: column;
+.table-holder {
+  flex-direction: column;
+  justify-content: flex-end;
 }
 </style>
