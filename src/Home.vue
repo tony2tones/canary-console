4c484398b716b969670688b8f80bda9473011d36
Anthony <anthonymarques@outlook.com>
2021-11-19 21:40:05 +0200

    added axios for api handling

--- src/Home.vue
+++ src/Home.vue
@@ -1,23 +1,36 @@
 <template>
-  <h1>I'm Home SUCKA</h1>
- 
+  <div class="container">
+    <h1>I'm Alerts SUCKA</h1>
+
+    <div v-for="alert in alerts" v-bind:key="alert[0].key">
+      <p>{{ alert[0].description }}</p>
+      <p>{{ alert[0].description }}</p>
+      <p>{{ alert[0].dst_host }}</p>
+      <p>{{ alert[0].src_host }}</p>
+      <p>{{ alert[0].created }}</p>
+      <p>{{ alert[0].node_id }}</p>
+    </div>
+  </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   data() {
     return {
+      alerts: [],
     };
   },
-  mounted() {
-    fetch(
-      "https://thinkst-frontend-resources.s3-eu-west-1.amazonaws.com/incidents/data.json"
-    )
-      .then((res) => res.json())
-      .then((data) => (console.log(data)))
-    //   console.log('received data ', data)
-    //   console.log('received data ', this.data)
-      .catch((err) => console.log(err.message));
+  created: function () {
+    axios
+      .get(
+        "https://thinkst-frontend-resources.s3-eu-west-1.amazonaws.com/incidents/data.json"
+      )
+      .then((res) => {
+        this.alerts = res.data;
+        console.log("First pull through ", this.alerts.alerts);
+      });
   },
 };
 </script>
