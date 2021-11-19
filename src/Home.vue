926327c02c52d9b2bd238fa240a397de9c351632
Anthony <anthonymarques@outlook.com>
2021-11-19 20:34:44 +0200

    made my first API call

--- src/Home.vue
+++ src/Home.vue
@@ -1,12 +1,23 @@
 <template>
-    <h1>I'm Home SUCKA</h1>
+  <h1>I'm Home SUCKA</h1>
+ 
 </template>
 
 <script>
-
-export default ({
-    data() {
-        
-    },
-})
+export default {
+  data() {
+    return {
+    };
+  },
+  mounted() {
+    fetch(
+      "https://thinkst-frontend-resources.s3-eu-west-1.amazonaws.com/incidents/data.json"
+    )
+      .then((res) => res.json())
+      .then((data) => (console.log(data)))
+    //   console.log('received data ', data)
+    //   console.log('received data ', this.data)
+      .catch((err) => console.log(err.message));
+  },
+};
 </script>
