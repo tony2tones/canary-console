01974114c4d4ede68b0a2a3ca101e164fcb6ccca
Anthony <anthonymarques@outlook.com>
2021-11-21 15:04:51 +0200

    populated all alert data

--- src/components/Alerts.vue
+++ src/components/Alerts.vue
@@ -7,23 +7,25 @@
         <p>Description: {{ alert.description }}</p>
         <p>dst_host: {{ alert.dst_host }}</p>
         <p>src_host: {{ alert.src_host }}</p>
+        <p>Created date:</p>
+        <p>{{ alert.created_printable }}</p>
         <p>created: {{ alert.created_age }}</p>
         <hr />
+        <p>created time: {{ alert.created }}</p>
         <p>created age: {{ alert.created_age }}</p>
         <p>created in seconds:{{ alert.created_age_seconds }}</p>
-        <p>{{ alert.created_printable }}</p>
-        <p>{{ alert.dst_port }}</p>
-        <p>{{ alert.events_count }}</p>
-        <p>{{ alert.ip_address }}</p>
-        <p>{{ alert.ippers }}</p>
-        <p>{{ alert.local_time }}</p>
-        <p>{{ alert.logtype }}</p>
-        <p>{{ alert.mac_address }}</p>
-        <p>{{ alert.netmask }}</p>
-        <p>{{ alert.notified }}</p>
-        <p>{{ alert.src_host_reverse }}</p>
-        <p>{{ alert.src_port }}</p>
-        <p>{{ alert.updated }}</p>
+        <p>dst_port: {{ alert.dst_port }}</p>
+        <p>Event count: {{ alert.events_count }}</p>
+        <p>ip Address: {{ alert.ip_address }}</p>
+        <p>Ippers: {{ alert.ippers }}</p>
+        <p>local time: {{ alert.local_time }}</p>
+        <p>Logic type: {{ alert.logtype }}</p>
+        <p>Mac address: {{ alert.mac_address }}</p>
+        <p>Netmask: {{ alert.netmask }}</p>
+        <p>notified: {{ alert.notified }}</p>
+        <p>src_host reverse: {{ alert.src_host_reverse }}</p>
+        <p>src_port{{ alert.src_port }}</p>
+        <p>Update: {{ alert.updated }}</p>
 
       </div>
     </div>
