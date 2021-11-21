e2c87746b9d3bfe5ca6ed0becc8d39d51504fb1c
Anthony <anthonymarques@outlook.com>
2021-11-21 15:25:43 +0200

    added place holders

--- src/components/Alerts.vue
+++ src/components/Alerts.vue
@@ -3,29 +3,29 @@
     <h3>Alerts</h3>
     <div class="alerts">
       <div v-for="alert in allAlerts" v-bind:key="alert.node_id" class="alert">
-        <p>Key: {{ alert.key }}</p>
-        <p>Description: {{ alert.description }}</p>
-        <p>dst_host: {{ alert.dst_host }}</p>
-        <p>src_host: {{ alert.src_host }}</p>
+        <p>Key: {{ alert.key || "not found" }}</p>
+        <p>Description: {{ alert.description || "not found" }}</p>
+        <p>dst_host: {{ alert.dst_host || "not found" }}</p>
+        <p>src_host: {{ alert.src_host || "not found" }}</p>
         <p>Created date:</p>
-        <p>{{ alert.created_printable }}</p>
-        <p>created: {{ alert.created_age }}</p>
+        <p>{{ alert.created_printable || "not found" }}</p>
+        <p>created: {{ alert.created_age || "not found" }}</p>
         <hr />
-        <p>created time: {{ alert.created }}</p>
-        <p>created age: {{ alert.created_age }}</p>
-        <p>created in seconds:{{ alert.created_age_seconds }}</p>
-        <p>dst_port: {{ alert.dst_port }}</p>
-        <p>Event count: {{ alert.events_count }}</p>
-        <p>ip Address: {{ alert.ip_address }}</p>
-        <p>Ippers: {{ alert.ippers }}</p>
-        <p>local time: {{ alert.local_time }}</p>
-        <p>Logic type: {{ alert.logtype }}</p>
-        <p>Mac address: {{ alert.mac_address }}</p>
-        <p>Netmask: {{ alert.netmask }}</p>
-        <p>notified: {{ alert.notified }}</p>
-        <p>src_host reverse: {{ alert.src_host_reverse }}</p>
-        <p>src_port{{ alert.src_port }}</p>
-        <p>Update: {{ alert.updated }}</p>
+        <p>created time: {{ alert.created || "not found" }}</p>
+        <p>created age: {{ alert.created_age || "not found" }}</p>
+        <p>created in seconds:{{ alert.created_age_seconds || "not found" }}</p>
+        <p>dst_port: {{ alert.dst_port || "not found" }}</p>
+        <p>Event count: {{ alert.events_count || "not found" }}</p>
+        <p>ip Address: {{ alert.ip_address || "not found" }}</p>
+        <p>Ippers: {{ alert.ippers || "not found" }}</p>
+        <p>local time: {{ alert.local_time || "not found" }}</p>
+        <p>Logic type: {{ alert.logtype || "not found" }}</p>
+        <p>Mac address: {{ alert.mac_address || "not found" }}</p>
+        <p>Netmask: {{ alert.netmask || "not found" }}</p>
+        <p>notified: {{ alert.notified || "not found" }}</p>
+        <p>src_host reverse: {{ alert.src_host_reverse || "not found" }}</p>
+        <p>src_port{{ alert.src_port || "not found" }}</p>
+        <p>Update: {{ alert.updated || "not found" }}</p>
 
       </div>
     </div>
